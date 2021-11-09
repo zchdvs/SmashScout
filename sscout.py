ee50ef9f7ac9c64b042d46f2665d6f2025b059ba
William Nguyen <nguyew2@uw.edu>
2021-11-08 21:13:03 -0800

    more queries

--- sscout.py
+++ sscout.py
@@ -2,6 +2,7 @@ import apitoken
 
 smashGG_Token = apitoken.smashGG_Token
 
+#https://smash.gg/tournament/port-priority-6/details
 # smashGG graphQL Ex:
 # query SocalTournaments($perPage: Int, $coordinates: String!, $radius: String!) {
 #   tournaments(query: {
@@ -25,6 +26,7 @@ smashGG_Token = apitoken.smashGG_Token
 #   "coordinates": "33.7454725,-117.86765300000002",
 #   "radius": "50mi"
 # }
+
 
 
 
@@ -59,5 +61,176 @@ query = gql(
   }
 }
 """)
-result = client.execute(query)
+
+
+#let's start with getting Port Priority 6
+
+testTournamentQuery = """
+{
+  tournaments(query:{
+    perPage: 5
+    filter: {
+      addrState: "WA"
+      afterDate: 1635145200
+      beforeDate: 1635836400
+    }
+  }) {
+    nodes 
+    {
+      id
+      countryCode
+      name
+      numAttendees
+      slug
+      venueName
+      venueAddress
+    }
+  }
+}
+"""
+
+"""
+query TournamentsByState($perPage: Int, $state: String!) {
+  tournaments(query: {
+    perPage: $perPage
+    filter: {
+      addrState: $state
+    }
+  }) {
+    nodes {
+      id
+      name
+      addrState
+    }
+  }
+},
+{
+  "perPage": 4,
+  "state": "CT"
+}
+"""
+
+#let's try getting user
+testQueryUser = """
+{
+  user(query: 
+  {
+
+  })
+
+}
+"""
+
+testZachQuery = """
+{tournaments(query: {
+    perPage: 4
+    filter: {
+      addrState: "WA"
+    }
+  }) 
+  {
+    nodes 
+    {
+      id
+      name
+      city
+      participants(query: {
+        perPage: 25,
+      }) 
+      {
+      nodes{
+        user{
+          slug
+          player{
+            gamerTag
+          }
+          id
+          name
+          location{
+            city
+          }
+          authorizations{
+          externalUsername
+          type
+          }
+        }
+      }
+      }
+    }
+  }
+}
+"""
+
+testPortPriorityQuery = """
+{
+  tournaments(query: {
+    perPage: 1
+    page: 1
+    filter:{
+      id: 211228
+    }
+  }) 
+  {
+  nodes
+    {
+      name
+      slug
+      addrState
+      city
+      countryCode
+      endAt
+      links
+      {
+        facebook
+        discord
+      }
+      participants(query: {
+        perPage: 100
+        page: 2
+      })
+      {
+        nodes
+        {
+          user
+          {
+            id
+            name
+            slug
+            player
+            {
+              id
+              gamerTag
+            }
+            authorizations
+            {
+              externalUsername
+              type
+              url
+            }            
+          }
+          contactInfo
+          {
+            id
+            city
+            country
+            countryId
+            name
+            nameFirst
+            nameLast
+            state
+            stateId
+            zipcode
+          }
+          connectedAccounts
+          email
+        }
+      }        
+    }
+  }
+}
+"""
+
+result = client.execute(gql(testPortPriorityQuery))
 print(result)
+
+#eventually raw JSON FORM and then we can just stringify
